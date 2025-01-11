235bfe30920ce586be9a5f0587e405f612787ba9
amellalalex <github@alexamellal.com>
2025-01-11 13:53:00 -0400

    Setup loop around first ex

--- main.c
+++ main.c
@@ -3,27 +3,32 @@
 main(void)
 {
 	int ch;
+	int done;
 
 	initscr();
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
 
-	printw("Type any character to see it in bold\n");
-	ch = getch();
+	done = 0;
+	while(!done) {
+		printw("Type any character to see it in bold\n");
+		ch = getch();
 
-	switch(ch) {
-		case KEY_F(1):
-			printw("Pressed the F1 key");
-			break;
-		default:
-			printw("The pressed key is ");
-			attron(A_BOLD);
-			printw("%c", ch);
-			attroff(A_BOLD);
+		switch(ch) {
+			case KEY_F(1):
+				printw("Pressed the F1 key");
+				break;
+			default:
+				printw("The pressed key is ");
+				attron(A_BOLD);
+				printw("%c", ch);
+				attroff(A_BOLD);
+		}
+
+		refresh();
 	}
 
-	refresh();
 	getch();
 	endwin();
 }
