3e5c42f83e2c402a56431060ff8ac0b24d07dbdc
amellalalex <github@alexamellal.com>
2025-01-11 13:52:42 -0400

    Got ncurses example working

--- main.c
+++ main.c
@@ -2,5 +2,28 @@
 
 main(void)
 {
+	int ch;
 
+	initscr();
+	raw();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	printw("Type any character to see it in bold\n");
+	ch = getch();
+
+	switch(ch) {
+		case KEY_F(1):
+			printw("Pressed the F1 key");
+			break;
+		default:
+			printw("The pressed key is ");
+			attron(A_BOLD);
+			printw("%c", ch);
+			attroff(A_BOLD);
+	}
+
+	refresh();
+	getch();
+	endwin();
 }
