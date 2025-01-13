a10ece5925fb224e000d8d79820f97edb081f7c3
amellalalex <github@alexamellal.com>
2025-01-13 12:01:49 -0400

    Added colours to setup

--- main.c
+++ main.c
@@ -3,11 +3,18 @@
 void
 setup(void)
 {
+	/* Core Setup */
 
 	initscr();
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
+
+	/* Colours */
+	if(has_colors()) {
+		start_color();
+		init_pair(1, COLOR_MAGENTA, COLOR_WHITE);
+	}
 
 }
 
