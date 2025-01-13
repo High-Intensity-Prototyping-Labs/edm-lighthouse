4ecfe16f78e576f3b924adcaa3dccfedb80dc265
amellalalex <github@alexamellal.com>
2025-01-13 12:22:40 -0400

    Added div status bar draw func

--- main.c
+++ main.c
@@ -38,6 +38,42 @@ draw_statusbar(char *status_text)
 
 }
 
+void
+draw_div_statusbar(char *div1, char *div2, char *div3)
+{
+
+	size_t 	div_len,
+			div1_len,
+			div2_len,
+			div3_len,
+			pad1_len,
+			pad2_len,
+			pad3_len
+			;
+
+	char status_text[COLS+1];
+
+	/* Caclulate divisional markers */
+	div_len = COLS / 3;
+	div1_len = (div1 == NULL) ? 0 : strlen(div1);
+	div2_len = (div2 == NULL) ? 0 : strlen(div2);
+	div3_len = (div3 == NULL) ? 0 : strlen(div3);
+	pad1_len = div_len - div1_len;
+	pad2_len = div_len - div2_len;
+	pad3_len = div_len - div3_len;
+
+
+	/* Place status texts */
+	status_text[COLS] = '\0';
+	memset(status_text, ' ', COLS);
+	memcpy(&status_text[0*div_len], div1, div1_len);
+	memcpy(&status_text[1*div_len], div2, div2_len);
+	memcpy(&status_text[2*div_len], div3, div3_len);
+
+	/* Draw status bar */
+	draw_statusbar(status_text);
+}
+
 main(void)
 {
 	int ch;
